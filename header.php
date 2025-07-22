3066c7d7a930ea86a6699135d0afcfaf5bde7b10
hmaster20 <hmaster20@gmail.com>
2025-07-22 14:18:14 +0300

    contact | i class, css+sha, описание шрифтаб требования PHP & WP

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-RZi67uzBCZmVA1n4z85Q7E6I7Rc1SVpZPAVO0b1B1Zrdgm5WEAfU5gH7Nd2Ab8H3pzw" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <?php wp_head(); ?>
     <style>
         .site-logo {
@@ -27,8 +27,8 @@
             </div>
             <div class="contact-info">
                 <a href="tel:+1234567890">+1 234 567 890</a>
-                <a href="https://wa.me/1234567890" class="whatsapp"></a>
-                <a href="https://t.me/username" class="telegram"></a>
+                <a href="https://wa.me/1234567890" class="whatsapp"><i class="fab fa-whatsapp"></i></a>
+                <a href="https://t.me/username" class="telegram"><i class="fab fa-telegram-plane"></i></a>
             </div>
         </div>
         <nav class="main-navigation">
